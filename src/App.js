5e69763478f37ad0f40d3d53fc3294e428ac3716
madhuraothota <madhuraothota@gmail.com>
2022-08-27 11:58:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Quiz from './Quiz';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Quiz/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
